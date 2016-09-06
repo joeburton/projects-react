09f080efc0cbba6bf6e93e4f020ed139a6f30310
joe burton <joeburton@gmail.com>
2016-09-06 22:26:03 +0100

    space

--- client-dev/app/reducers/projects.js
+++ client-dev/app/reducers/projects.js
@@ -8,41 +8,41 @@ export default function(state = projectsInitialState, action) {
 
 		case 'GET_PROJECTS' :
 
-		var newState = Object.assign({}, state)
-		newState.projects = action.projects;
-		
+			var newState = Object.assign({}, state)
+			newState.projects = action.projects;
+			
 		return newState;
 
 		case 'ADD_PROJECT':
 
-		for (var i in state.projects) {
-			if (state.projects[i]['company'] == action.project.company)
-			console.log('company already exists: ', action.project.company);
-			break;
-		}
+			for (var i in state.projects) {
+				if (state.projects[i]['company'] == action.project.company)
+				console.log('company already exists: ', action.project.company);
+				break;
+			}
 
-		var newState = Object.assign({}, state);
-		newState.projects.push(action.project);
-		
+			var newState = Object.assign({}, state);
+			newState.projects.push(action.project);
+			
 		return newState;
 
 		case 'UPDATE_PROJECT':
 
-		return Object.assign({}, state, { projects: state.projects.map((project, index) => {
-			if (project._id === action.project._id) {
-				// update company name
-				project.company = action.project.company;
-				// update project values
-				project.projects[action.project.key] = {
-					description: action.project.description,
-					link: action.project.link,
-					project: action.project.project,
-					skills: action.project.skills
-				};
-			}
-			return project;
-			})
-		});
+			return Object.assign({}, state, { projects: state.projects.map((project, index) => {
+				if (project._id === action.project._id) {
+					// update company name
+					project.company = action.project.company;
+					// update project values
+					project.projects[action.project.key] = {
+						description: action.project.description,
+						link: action.project.link,
+						project: action.project.project,
+						skills: action.project.skills
+					};
+				}
+				return project;
+				})
+			});
 
 		default: return state;
 
