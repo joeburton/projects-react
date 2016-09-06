64f308277cdd7a63dbd8790cbb45330b86b818f7
joe burton <joeburton@gmail.com>
2016-09-06 22:13:19 +0100

    space

--- client-dev/app/api/projects.js
+++ client-dev/app/api/projects.js
@@ -15,68 +15,68 @@ let axiosAjax = {
         });
 
     },
-	addProject (data, projects) {
-		
-		let postData,
-			companyExists;
+    addProject (data, projects) {
+    	
+        let postData,
+            companyExists;
 
-		// Logic to check if the company already exists. 
-		// If it does add the project to the exisiting company projects array
-		// If it does not set a flag to add a new company.
-		for (var i in projects) {
-		    if (projects[i]['company'] === data.company) {
-				data.id = projects[i]['_id'];
-				postData = data;
-				companyExists = true;
-				break;
-		    } else {
-		    	companyExists = false;
-		    	postData = data;
-		    }
-		}
+        // Logic to check if the company already exists. 
+        // If it does add the project to the exisiting company projects array
+        // If it does not set a flag to add a new company.
+        for (var i in projects) {
+            if (projects[i]['company'] === data.company) {
+                data.id = projects[i]['_id'];
+                postData = data;
+                companyExists = true;
+                break;
+            } else {
+                companyExists = false;
+                postData = data;
+            }
+        }
 
-		let addType = (companyExists) ? 'addproject' : 'addcompany';
+        let addType = (companyExists) ? 'addproject' : 'addcompany';
 
-		axios.post('http://localhost:3000/' + addType, postData)
-		.then(function (response) {
-			store.dispatch({
-	            type: 'GET_PROJECTS',
-	            projects: response.data
-	        });
-		})
-		.catch(function (error) {
-			console.log(error);
-		});
+        axios.post('http://localhost:3000/' + addType, postData)
+        .then(function (response) {
+            store.dispatch({
+            type: 'GET_PROJECTS',
+            projects: response.data
+            });
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 
-	},
-	updateProject (data) {
+    },
+    updateProject (data) {
 
-		axios.post('http://localhost:3000/updateproject', data)
-		.then(function (response) {
-			console.log(response);
-		})
-		.catch(function (error) {
-			console.log(error);
-		});
+        axios.post('http://localhost:3000/updateproject', data)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 
-	},
-	deleteProject (companyId, projectId, projectListItemsLength) {
+    },
+    deleteProject (companyId, projectId, projectListItemsLength) {
 
-		let postData = {
-			companyId, 
-			projectId,
-			projectListItemsLength
-		};
+        let postData = {
+            companyId, 
+            projectId,
+            projectListItemsLength
+        };
 
-		axios.post('http://localhost:3000/deleteproject', postData)
-		.then(function (response) {
-			console.log(response);
-		})
-		.catch(function (error) {
-			console.log(error);
-		});
-		
-	}
+        axios.post('http://localhost:3000/deleteproject', postData)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    	
+    }
 };
 
 export default axiosAjax;
