dc363c84b4c542d07f237139d1996dac2fe774d2
Joe Burton <Burton.J@cambridgeassessment.org.uk>
2016-09-07 11:57:14 +0100

    more formatting

--- client-dev/app/ui/edit-project-container.js
+++ client-dev/app/ui/edit-project-container.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import EditProject from './edit-project';
 import { connect } from 'react-redux';
-import store from 'store';
 import axiosAjax from 'api/projects';
 
 const EditProjectContainer = React.createClass({
