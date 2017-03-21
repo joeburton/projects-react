06e9b742b40245fa561e6b37eed4217566647515
Joe Burton <Burton.J@cambridgeassessment.org.uk>
2017-03-21 12:59:52 +0000

    clean up files and modify bootstrap

--- app/js/ui/projects.js
+++ app/js/ui/projects.js
@@ -30,7 +30,10 @@ export default React.createClass({
                             <div>Skills: <span className='project-item skills'> {proj.skills} </span></div>
                             <div>Description: <span className='project-item description'> {proj.description} </span></div>
                             { this.props.loggedin &&
-                                <div className="admin-controls btn btn-default"><a href='#' data-company-id={id} data-project-id={proj.id} data-project={proj.project} data-project-key={i} onClick={this.props.openEditInput}>edit</a> | <a href='#' data-company-id={id}  data-project-id={proj.id} data-project={proj.project} data-project-key={i} onClick={this.deleteProject}>delete</a></div>
+                                <div className="admin-controls">
+                                    <a className="btn btn-default btn-primary btn-edit" href='#' data-company-id={id} data-project-id={proj.id} data-project={proj.project} data-project-key={i} onClick={this.props.openEditInput}>EDIT</a>
+                                    <a className="btn btn-default btn-danger" href='#' data-company-id={id}  data-project-id={proj.id} data-project={proj.project} data-project-key={i} onClick={this.deleteProject}>DELETE</a>
+                                </div>
                               }
                         </li>
                     );
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,18 @@
 var gulp = require('gulp');
-var webpack = require('webpack-stream');
-var serve = require('gulp-serve');
-
-// all for the love of squished css
 var concat = require('gulp-concat');
 var minifyCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var imagemin = require('gulp-imagemin');
-
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
-
 var uglify = require('gulp-uglify');
 var streamify = require('gulp-streamify');
-
 var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
+
+// Optimize images
 gulp.task('process-images', () => {
 
     const ret = gulp.src(['./app/images/**/*.+(png|jpg|jpeg|gif|svg)'])
@@ -29,18 +25,10 @@ gulp.task('process-images', () => {
     return ret;
 });
 
-// Webpack - not currently used
-// gulp.task('webpack', function () {
-//     return gulp.src('./app/js/app.js')
-//         .pipe(webpack(require('./webpack.config.js')))
-//         .pipe(gulp.dest('./'));
-// });
 
-// Build js - [not currently used]
-// Turn debug to false to remove source maps
 // http://egorsmirnov.me/2015/05/22/react-and-es6-part1.html
 gulp.task('js-dev', ['js-bootstrap'], function () {
-
+    // Turn debug to false to remove source maps
     return browserify({ entries: './app/js/app.js', extensions: ['.js'], debug: true })
         .transform('babelify', { presets: ['es2015', 'react'] })
         .bundle()
@@ -48,6 +36,8 @@ gulp.task('js-dev', ['js-bootstrap'], function () {
         .pipe(gulp.dest('public/js'));
 });
 
+
+// Build js for production
 gulp.task('js-prod', ['js-bootstrap'], function () {
     // make sure debug is false otherwise output JS is MASSIVE!
     return browserify({ entries: './app/js/app.js', extensions: ['.js'], debug: false })
@@ -58,38 +48,63 @@ gulp.task('js-prod', ['js-bootstrap'], function () {
         .pipe(gulp.dest('public/js'));
 });
 
+
+// Include bootstrap JS
 gulp.task('js-bootstrap', function () {
     gulp.src('node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js')
         .pipe(gulp.dest('public/js'));
 });
+
 
 // CSS minification
 gulp.task('sass', function () {
 
     return sass('./app/css/app.scss', {
         style: 'compressed',
+        sourcemap: true,
         loadPath: ['node_modules/bootstrap-sass/assets/stylesheets']
     })
         .pipe(concat('style.min.css'))
         .pipe(minifyCSS())
+        .pipe(sourcemaps.write())
+		// for file sourcemaps
+		.pipe(sourcemaps.write('maps', {
+			includeContent: false,
+			sourceRoot: 'source'
+		}))
         .pipe(gulp.dest('./public/css/'))
         .pipe(gulp.dest('./app/css/'));
 
 });
 
+// Fonts
+gulp.task('fonts', function() {
+    return gulp.src('node_modules/bootstrap-sass/assets/fonts/**/*')
+    .pipe(gulp.dest('./public/fonts'));
+});
+
 
 // CSS minification
 gulp.task('css', function () {
-    
+
     gulp.src('./app/css/style.min.css')
         .pipe(gulp.dest('./public/css'))
 });
 
+
+// Watch
 gulp.task('watch', function () {
-    gulp.watch('./app/**/*', ['js-dev', 'sass', 'process-images'])
+    gulp.watch('./app/**/*', ['js-dev', 'sass', 'fonts', 'process-images'])
 });
-    
+
+
+// Default
 gulp.task('default', ['watch']);
 
-//@TODO issue with gulp-ruby-sass on the ser ver needing ruby Gem installed... So compile sass locally then push css.
-gulp.task('production', ['js-prod', 'css', 'process-images']);
+
+// Dev build
+gulp.task('dev', ['js-dev', 'sass', 'fonts', 'process-images']);
+
+
+// Dev production: @TODO issue with gulp-ruby-sass on the server needing ruby Gem installed... So compile sass locally then push css.
+gulp.task('production', ['js-prod', 'css', 'fonts', 'process-images']);
