9a1e2de7b5269bbf47734ab895b1a2fe0eaf767e
joe burton <joeburton@gmail.com>
2017-11-04 16:14:30 +0000

    notes

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
         ]
     },
     devtool: "source-map", // any "source-map"-like devtool is possible
-    // devtool: 'inline-source-map', // don't use for production
+    // devtool: 'inline-source-map', // don't use for production; good for debugging test specs
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
