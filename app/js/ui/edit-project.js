c4e134f0a23e424b173cf4d5147fd6e66c992b12
joe burton <joeburton@gmail.com>
2017-12-28 20:17:49 +0000

    adding @todos

--- app/js/ui/edit-project.js
+++ app/js/ui/edit-project.js
@@ -17,55 +17,24 @@ class EditProject extends React.Component {
 
         this.setValue = this.setValue.bind(this);
         this.handleChange = this.handleChange.bind(this);
-        this.getValues = this.getValues.bind(this);
-    }
-    populateForm() {
-
-        // // edit form fields elements
-        // let editProjectEle = document.querySelector('.edit-project');
-        // let fieldsWrapper = editProjectEle.querySelector('.edit-fields');
-        // let company = editProjectEle.querySelector('.company');
-        // let project = editProjectEle.querySelector('.project-name');
-        // let link = editProjectEle.querySelector('.link');
-        // let skills = editProjectEle.querySelector('.skills');
-        // let description = editProjectEle.querySelector('.description');
-
-        // // get selected parent DOM element
-        // let projectContainerUL = e.target.parentElement.parentNode.parentNode;
-
-        // // get selected project values
-        // let currentValueCompany = projectContainerUL.parentNode.querySelector('.company-name').textContent.split(':')[1];
-        // let currentValueProject = projectContainerUL.getElementsByClassName('project')[0].textContent;
-        // let currentValueLink = projectContainerUL.getElementsByClassName('link')[0].textContent;
-        // let currentValueSkills = projectContainerUL.getElementsByClassName('skills')[0].textContent;
-        // let currentValueDescription = projectContainerUL.getElementsByClassName('description')[0].textContent;
-
-        // // set edit field data attribute values
-        // fieldsWrapper.setAttribute('data-company-id', e.target.getAttribute('data-company-id'));
-        // fieldsWrapper.setAttribute('data-project-id', e.target.getAttribute('data-project-id'));
-
-        // // set edit form field values
-        // company.value = currentValueCompany.trim();
-        // project.value = currentValueProject.trim();
-        // link.value = currentValueLink.trim();
-        // skills.value = currentValueSkills.trim();
-        // description.value = currentValueDescription.trim();
-
-        console.log('component did mount');
     }
     componentDidUpdate() {
 
         (this.props.open) ? $('#edit').modal('show') : $('#edit').modal('hide');
 
+        let project = _.find(this.props.company.projects, 'id', this.props.projectId);
+
+        // @todo update state and populate form
+
         console.log('company id', this.props.companyId);
         console.log('project id', this.props.projectId);
         console.log('company', this.props.company);
 
-        let project = _.find(this.props.company.projects, 'id', this.props.projectId);
-
         console.log('project', project);
     }
-    setValue(e) {
+    update(e) {
+
+        // @todo get values from state
 
         // send object to dispatch in container component.
         this.props.disptachProjectUpdate(e, {
@@ -90,30 +59,6 @@ class EditProject extends React.Component {
         formValues[name] = value;
 
         this.setState({ formValues });
-    }
-    getValues() {
-
-        // wrapping el
-        let projectEl = document.querySelector('.edit-fields');
-
-        // get elements
-        let companyId = projectEl.getAttribute('data-company-id');
-        let projectId = projectEl.getAttribute('data-project-id');
-        // let company = projectEl.querySelector('.company');
-        // let name = projectEl.querySelector('.project-name');
-        // let link = projectEl.querySelector('.link');
-        // let skills = projectEl.querySelector('.skills');
-        // let description = projectEl.querySelector('.description');
-
-        return {
-            companyId: companyId,
-            projectId: projectId,
-            company: this.state.company,
-            name: this.state.name,
-            link: this.state.link,
-            skills: this.state.skills,
-            description: this.state.description
-        };
     }
     render(state) {
         return (
@@ -143,7 +88,7 @@ class EditProject extends React.Component {
                         </div>
                         <div className="modal-footer">
                             <button name='button' className="btn btn-default" onClick={this.props.closeEditProjectModal}>CANCEL</button>
-                            <button name='button' className="btn btn-primary" onClick={this.setValue}>UPDATE</button>
+                            <button name='button' className="btn btn-primary" onClick={this.update}>UPDATE</button>
                         </div>
                     </div>
                 </div>
